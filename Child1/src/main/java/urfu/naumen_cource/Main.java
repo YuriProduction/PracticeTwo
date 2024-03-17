014337be68b59aa6f28c0a921ae92cda405225c6
Jurex <jurexda@yandex.ru>
2024-03-17 21:19:21 +0500

    Homework2 done.

---
+++ Child1/src/main/java/urfu/naumen_cource/Main.java
@@ -0,0 +1,15 @@
+package urfu.naumen_cource;
+
+
+import org.apache.commons.io.IOUtils;
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        final IOUtils ioUtils = new IOUtils();
+
+    }
+}
---
+++ src/main/java/urfu/naumen_cource/App.java
@@ -0,0 +1,13 @@
+package urfu.naumen_cource;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
